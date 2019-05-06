1f2c66604a1a6aa7de162512e18dc7ed805386a9
Codemann12 <fbcedric@yahoo.fr>
2019-05-06 11:44:31 +0200

    Rename index -> CanvasTimeSeriesPlot

---
+++ src/demo.ts
@@ -0,0 +1,15 @@
+import{CanvasTimeSeriesPlot} from './CanvasTimeSeriesPlot';
+import * as d3 from 'd3';
+
+function getDemoPlotSize(): Array<number> {
+	return [window.innerWidth-100, Math.round(0.45*(window.innerWidth-100))];
+}
+
+function addDays(date: Date, days: number): Date {
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+// var plot2 = new CanvasTimeSeriesPlot(d3.select("#maincontainer"), getDemoPlotSize(), {
+//     yAxisLabel: "Voltage [V]"
+// });
